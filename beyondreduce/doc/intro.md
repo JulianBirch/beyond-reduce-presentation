aaa7dea1ea11908654d8ec824b644728e50642b6
Julian Birch <julian.birch@gmail.com>
2015-11-13 21:47:06 +0000

    Initial cut

---
+++ beyondreduce/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to beyondreduce
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ beyondreduce/test/beyondreduce/core_test.clj
@@ -0,0 +1,7 @@
+(ns beyondreduce.core-test
+  (:require [clojure.test :refer :all]
+            [beyondreduce.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
